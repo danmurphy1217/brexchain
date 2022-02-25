d7ff23b0b9eab2a9f74d6dc59385d559e8f1cdc0
Dan Murphy <danielmurph8@gmail.com>
2022-02-25 08:45:53 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,9 @@
+# install starport CLI
 brew install tendermint/tap/starport
+
+# add go to your path
+echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> ~/.vimrc
+
+# download repo and init blockchain w/ fresh genesis state
 git clone https://github.com/danmurphy1217/brexchain.git
 cd brexchain && starport chain serve -r
