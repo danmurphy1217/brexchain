a62f57f2a67f2d61d5bae1c127e3f722a2c97e67
Dan Murphy <danielmurph8@gmail.com>
2022-02-25 08:11:25 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 brew install tendermint/tap/starport
-wget https://github.com/danmurphy1217/brexchain.git
+git clone https://github.com/danmurphy1217/brexchain.git
 cd brexchain && starport chain serve -r
